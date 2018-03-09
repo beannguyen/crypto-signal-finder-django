f48171c191d6f7b26ad3ccfd682c0cfd0e3e2363
Bean Nguyen <12110009@student.hcmute.edu.vn>
2018-03-09 15:20:35 +0700

    update idle killer

--- idle_killer.py
+++ idle_killer.py
@@ -2,14 +2,17 @@ import psycopg2
 import traceback
 import time
 
-
 try:
     conn = psycopg2.connect("dbname='bsf_test1' user='killer' host='localhost' password='Th3NeWorld@@@1893'")
     cur = conn.cursor()
-    sql = "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'bsf_test1' AND pid <> pg_backend_pid() AND state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled') AND state_change < current_timestamp - INTERVAL '15' MINUTE;"
+    sql = "﻿SELECT pg_terminate_backend(pid) " \
+          "FROM pg_stat_activity " \
+          "WHERE datname = 'bsf_test1' " \
+          "AND state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')" \
+          "AND pid <> pg_backend_pid()"
 
     while True:
         cur.execute(sql)
-        time.sleep(5 * 60)
+        time.sleep(1 * 60)
 except:
     traceback.print_exc()
