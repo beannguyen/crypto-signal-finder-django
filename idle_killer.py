35f4f1d25f65ff7cbc6f85d3a4fae6bd2c08f507
root <root@ftech.ftech>
2018-03-10 10:30:42 +0700

    idle killer

--- idle_killer.py
+++ idle_killer.py
@@ -3,19 +3,23 @@ import traceback
 import time
 
 try:
-    INTERVAL_TIME = 10
+    INTERVAL_TIME = 30
 
-    conn = psycopg2.connect("dbname='bsf_test1' user='killer' host='localhost' password='Th3NeWorld@@@1893'")
-    cur = conn.cursor()
-    sql = "﻿SELECT pg_terminate_backend(pid) " \
+    sql = "SELECT pg_terminate_backend(pid) " \
           "FROM pg_stat_activity " \
           "WHERE datname = 'bsf_test1' " \
           "AND state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')" \
           "AND pid <> pg_backend_pid()" \
           "AND state_change < current_timestamp - INTERVAL '{}' MINUTE;".format(INTERVAL_TIME)
 
+    time.sleep(30 * 60)
+
     while True:
+        start_time = time.time()
+        conn = psycopg2.connect("dbname='bsf_test1' user='killer' host='localhost' password='Th3NeWorld@@@1893'")
+        cur = conn.cursor()
         cur.execute(sql)
-        time.sleep(INTERVAL_TIME * 60)
+        print('killed process after {}s'.format(time.time() - start_time))
+        time.sleep((INTERVAL_TIME) * 2 * 60)
 except:
     traceback.print_exc()
