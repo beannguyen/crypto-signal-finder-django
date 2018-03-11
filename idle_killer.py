ca5602f04781d1a8896871eba0161e91efe635b5
root <root@ftech.ftech>
2018-03-11 15:56:53 +0700

    change idle killer logic

--- idle_killer.py
+++ idle_killer.py
@@ -3,11 +3,11 @@ import traceback
 import time
 
 conn = psycopg2.connect("dbname='bsf_test1' user='killer' host='localhost' password='Th3NeWorld@@@1893'")
-cur = conn.cursor()
 
 
 def count_all_activity():
-    sql = "﻿select count(*) FROM pg_stat_activity where datname = 'bsf_test1'"
+    cur = conn.cursor()
+    sql = "select count(*) FROM pg_stat_activity where datname = 'bsf_test1'"
     cur.execute(sql)
     rows = cur.fetchall()
     r = rows[0]
@@ -17,7 +17,7 @@ def count_all_activity():
 if __name__ == '__main__':
     try:
         INTERVAL_TIME = 30
-        SAFE_CONNECTIONS = 1000
+        SAFE_CONNECTIONS = 300
 
         sql = "SELECT pg_terminate_backend(pid) " \
               "FROM pg_stat_activity " \
@@ -25,15 +25,18 @@ if __name__ == '__main__':
               "AND usename <> 'killer' " \
               "AND state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')" \
               "AND pid <> pg_backend_pid()" \
-              "AND state_change < current_timestamp - INTERVAL '{}' MINUTE;".format(INTERVAL_TIME)
+              "AND state_change < current_timestamp - INTERVAL '{}' MINUTE;".format(INTERVAL_TIME - 15)
 
-        time.sleep(INTERVAL_TIME * 60)
+        # time.sleep(INTERVAL_TIME * 60)
 
         while True:
+            cur = conn.cursor()
             start_time = time.time()
-            if count_all_activity() >= SAFE_CONNECTIONS:
+            c = count_all_activity()
+            print('All activity: ', c)
+            if c >= SAFE_CONNECTIONS:
                 cur.execute(sql)
                 print('killed process after {}s'.format(time.time() - start_time))
-            time.sleep(INTERVAL_TIME * 60)
+            time.sleep(5 * 60)
     except:
         traceback.print_exc()
